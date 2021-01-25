1efdf51b4af3ccb3c4e3415704c5185d3fff71d1
AntoniKos <antoha.kostylev@mail.ru>
2021-01-25 20:38:13 +0300

    25/01/21 helicopter image

--- helicopter.py
+++ helicopter.py
@@ -1,103 +1,140 @@
-
 from graph import *
+
 windowSize(700, 700)
 canvasPos(5, 5)
 canvasSize(690, 690)
+image(0, 0, 'fon.gif')
 
-def helicopter(x,y):
-  global a, obj1, obj4
-  hel_body=[(x+10, y-23), (x+13, y-10),\
-            (x+13, y+10), (x+5, y+20),\
-            (x, y+45), (x-5, y+20),\
-            (x-13, y+10), (x-13, y-10),\
-            (x-10, y-23), (x+10, y-23)]
-  """корпус вертолета"""
-  brushColor("yellow")
-  penSize(2)
-  obj1=polygon(hel_body)
-  """лыжи"""
-  obj2=line(x+15, y-20, x+15, y+20)
-  obj3=line(x-15, y-20, x-15, y+20)
-  """пропеллер"""
-  brushColor("white")
-  penSize(1)
-  obj4=circle(x,y,20)
-  brushColor("black")
-  obj5=circle(x,y,2)
-  obj6=line(x,y,x+20,y)
-  obj7=line(x,y,x,y+20)
-  """малый пропеллер"""
-  obj8=line(x,y+45,x+5,y+45)
-  obj9=line(x+5,y+40,x+5,y+50)
-  a=[obj1, obj2, obj3, obj9, obj5, obj6, obj7, obj8, obj4]
- 
+
+def helicopter(x, y):
+    global a, obj1, obj4
+    hel_body = [(x + 10, y - 23), (x + 13, y - 10), \
+                (x + 13, y + 10), (x + 5, y + 20), \
+                (x, y + 45), (x - 5, y + 20), \
+                (x - 13, y + 10), (x - 13, y - 10), \
+                (x - 10, y - 23), (x + 10, y - 23)]
+    """корпус вертолета"""
+    brushColor("yellow")
+    penSize(2)
+    obj1 = polygon(hel_body)
+    """лыжи"""
+    obj2 = line(x + 15, y - 20, x + 15, y + 20)
+    obj3 = line(x - 15, y - 20, x - 15, y + 20)
+    """пропеллер"""
+    brushColor("white")
+    penSize(1)
+    obj4 = circle(x, y, 20)
+    brushColor("black")
+    obj5 = circle(x, y, 2)
+    obj6 = line(x, y, x + 20, y)
+    obj7 = line(x, y, x, y + 20)
+    """малый пропеллер"""
+    obj8 = line(x, y + 45, x + 5, y + 45)
+    obj9 = line(x + 5, y + 40, x + 5, y + 50)
+    a = [obj1, obj2, obj3, obj9, obj5, obj6, obj7, obj8, obj4]
+
+
 def keyPressed(event):
-  global dx, dy
-  if event.keycode == VK_LEFT:
-    dx = -5; dy = 0
-  elif event.keycode == VK_RIGHT:
-    dx = 5; dy = 0
-  elif event.keycode == VK_UP:
-    dx = 0; dy = -5
-  elif event.keycode == VK_DOWN:
-    dx = 0; dy = 5
-  elif event.keycode == VK_SPACE:
-    dx = dy = 0
-  elif event.keycode == VK_ESCAPE:
-    close()
-    
+    global dx, dy
+    if event.keycode == VK_LEFT:
+        dx = -5;
+        dy = 0
+    elif event.keycode == VK_RIGHT:
+        dx = 5;
+        dy = 0
+    elif event.keycode == VK_UP:
+        dx = 0;
+        dy = -5
+    elif event.keycode == VK_DOWN:
+        dx = 0;
+        dy = 5
+    elif event.keycode == VK_SPACE:
+        dx = dy = 0
+    elif event.keycode == VK_ESCAPE:
+        close()
+
+
 def update():
-  dx1=dx
-  dy1=dy
-  for i in a:
-      x_coord = coords(obj4)[0]
-      y_coord = coords(obj4)[1]
-      if x_coord <= 0:
-        if 5 < y_coord < 619:
-          if dx < 0: dx1 = 0
-          elif dx >= 0: dx1 = dx; dy1 = dy
+    dx1 = dx
+    dy1 = dy
+    for i in a:
+        x_coord = coords(obj4)[0]
+        y_coord = coords(obj4)[1]
+        if x_coord <= 0:
+            if 5 < y_coord < 619:
+                if dx < 0:
+                    dx1 = 0
+                elif dx >= 0:
+                    dx1 = dx; dy1 = dy
+                moveObjectBy(i, dx1, dy1)
 
-        elif y_coord <= 5:
-          if dx < 0 or dy < 0: dx1 = 0; dy1 = 0
-          elif dy > 0: dx1 = 0; dy1 = dy
-          elif dx > 0: dx1 = dx; dy1 = 0
+            elif y_coord <= 5:
+                if dx < 0 or dy < 0:
+                    dx1 = 0; dy1 = 0
+                elif dy > 0:
+                    dx1 = 0; dy1 = dy
+                elif dx > 0:
+                    dx1 = dx; dy1 = 0
+                moveObjectBy(i, dx1, dy1)
 
-        elif y_coord >= 619:
-          if dx < 0 or dy > 0: dx1 = 0; dy1 = 0
-          elif dy < 0: dx1 = 0; dy1 = dy
-          elif dx > 0: dx1 = dx; dy1 = 0
+            elif y_coord >= 619:
+                if dx < 0 or dy > 0:
+                    dx1 = 0; dy1 = 0
+                elif dy < 0:
+                    dx1 = 0; dy1 = dy
+                elif dx > 0:
+                    dx1 = dx; dy1 = 0
+                moveObjectBy(i, dx1, dy1)
 
-      elif x_coord >= 650:
-        if 5 < y_coord < 619:
-          if dx > 0: dx1 = 0
-          elif dx <= 0: dx1 = dx; dy1 = dy
+        elif x_coord >= 650:
+            if 5 < y_coord < 619:
+                if dx > 0:
+                    dx1 = 0
+                elif dx <= 0:
+                    dx1 = dx; dy1 = dy
+                moveObjectBy(i, dx1, dy1)
 
-        elif y_coord <= 5:
-          if dx > 0 or dy < 0: dx1 = 0; dy1 = 0
-          elif dy > 0: dx1 = 0; dy1 = dy
-          elif dx < 0: dx1 = dx; dy1 = 0
+            elif y_coord <= 5:
+                if dx > 0 or dy < 0:
+                    dx1 = 0; dy1 = 0
+                elif dy > 0:
+                    dx1 = 0; dy1 = dy
+                elif dx < 0:
+                    dx1 = dx; dy1 = 0
+                moveObjectBy(i, dx1, dy1)
 
-        elif y_coord >= 619:
-          if dx > 0 or dy > 0: dx1 = 0; dy1 = 0
-          elif dy < 0: dx1 = 0; dy1 = dy
-          elif dx < 0: dx1 = dx; dy1 = 0
+            elif y_coord >= 619:
+                if dx > 0 or dy > 0:
+                    dx1 = 0; dy1 = 0
+                elif dy < 0:
+                    dx1 = 0; dy1 = dy
+                elif dx < 0:
+                    dx1 = dx; dy1 = 0
+                moveObjectBy(i, dx1, dy1)
 
-      elif 0 < x_coord < 650 and y_coord <= 5:
-        if dy < 0: dy1 = 0
-        elif dy > 0: dx1 = dx; dy1 = dy
+        elif 0 < x_coord < 650 and y_coord <= 5:
+            if dy < 0:
+                dy1 = 0
+            elif dy >= 0:
+                dx1 = dx; dy1 = dy
+                moveObjectBy(i, dx1, dy1)
 
-      elif 0 < x_coord < 650 and y_coord >= 619:
-        if dy > 0: dy1 = 0
-        elif dy < 0: dx1 = dx; dy1 = dy
+        elif 0 < x_coord < 650 and y_coord >= 619:
+            if dy > 0:
+                dy1 = 0
+            elif dy <= 0:
+                dx1 = dx; dy1 = dy
+                moveObjectBy(i, dx1, dy1)
 
-      else:
-        moveObjectBy(i, dx, dy)
-      moveObjectBy(i, dx1, dy1)
- 
-    
-dx = 0;  dy = 0
+        else:
+            moveObjectBy(i, dx, dy)
 
-obj=helicopter(345,345)
+
+
+dx = 0
+dy = 0
+
+obj = helicopter(345, 345)
 
 onKey(keyPressed)
 onTimer(update, 50)
